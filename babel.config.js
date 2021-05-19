7a3a74b1132344c2cca8d957ab6fa2fac0e68ac7
John Qian <johnlongqian@gmail.com>
2021-05-18 22:11:53 -0700

    install styled-components babel plugin

--- babel.config.js
+++ babel.config.js
@@ -54,8 +54,11 @@ module.exports = (api) => {
       require('@babel/plugin-syntax-dynamic-import'),
       require('@babel/plugin-syntax-import-meta'),
       [require('@babel/plugin-proposal-class-properties'), { loose: true }],
-      [require("@babel/plugin-proposal-private-methods"), { loose: true }],
+      [require('@babel/plugin-proposal-private-methods'), { loose: true }],
       require('@babel/plugin-proposal-json-strings'),
+
+      // Added by me!
+      require('babel-plugin-styled-components'),
 
       ...(development ? developmentPlugins : productionPlugins),
     ],
